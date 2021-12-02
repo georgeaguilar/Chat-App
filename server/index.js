dfb1d02346f5b762aefb50bbe5b56ff91b230db5
georgeaguilar <georgeaguilar@htoamil.com>
2021-12-01 22:29:06 -0600

    make the server running in the port 5000

--- server/index.js
+++ server/index.js
@@ -4,3 +4,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
