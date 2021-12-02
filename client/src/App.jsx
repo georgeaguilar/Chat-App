e2396a723423c2199a0f8f8dad86c5058978ff17
georgeaguilar <georgeaguilar@htoamil.com>
2021-12-01 18:17:17 -0600

    add commponet teamchannellist

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React from 'react';
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
+import { ChannelContainer, ChannelListContainer } from './componets';
+
+import './App.css';
+
+const apiKey = 't8pts79hnpnp';
+
+const client = StreamChat.getInstance(apiKey);
+  
 const App = () => {
     return (
-        <div>
-            <h1>Chat Messaging App</h1>
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer />>
+            </Chat>
         </div>
     )
 }
