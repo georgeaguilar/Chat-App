916d14b8196e1307a51fae0161e82ed1538c76aa
georgeaguilar <georgeaguilar@htoamil.com>
2021-12-02 05:48:56 -0600

    editing some componets

--- client/src/componets/ChannelSearch.jsx
+++ client/src/componets/ChannelSearch.jsx
@@ -1,15 +1,38 @@
 import React, {useState, useEffect} from 'react';
-import { getChannel, useChatContext } from 'stream-chat-react';
+import {useChatContext } from 'stream-chat-react';
 
 import {SearchIcon} from '../assets';
 
-const ChannelSearch = () => {
+const ChannelSearch = ({setToggleContainer}) => {
+    const { client, setActiveChannel } = useChatContext();
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
+    const [teamChannels, setTeamChannels] = useState([]);
+    const [directChannels, setDirectChannels] = useState([]);
+
+    useEffect(() => {
+        if(!query) {
+            setTeamChannels([]);
+            setDirectChannels([]);
+        }
+    }, [query])
 
     const getChannels = async () => {
         try {
-            //TODO: fetch channels
+            const channelResponse = client.queryChannels({
+                type: 'team', 
+                name: { $autocomplete: text }, 
+                members: { $in: [client.userID]}
+            });
+            const userResponse = client.queryUsers({
+                id: { $ne: client.userID },
+                name: { $autocomplete: text }
+            })
+
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
+
+            if(channels.length) setTeamChannels(channels);
+            if(users.length) setDirectChannels(users);
         } catch(error) {
             setQuery('');
         }
@@ -22,6 +45,12 @@ const ChannelSearch = () => {
         setQuery(event.target.value)
         getChannels(event.target.value)
     }
+
+    const setChannel = (channel) => {
+        setQuery('');
+        setActiveChannel(channel);
+    }
+
     return (
         <div className="channel-search__container">
             <div className="channel-search__input__wrapper">
@@ -36,6 +65,16 @@ const ChannelSearch = () => {
                 onChange={onSearch}
                 />
             </div>
+            { query && (
+                <ResultsDropdown 
+                    teamChannels={teamChannels}
+                    directChannels={directChannels}
+                    loading={loading}
+                    setChannel={setChannel}
+                    setQuery={setQuery}
+                    setToggleContainer={setToggleContainer}
+                />
+            )}
         </div>
     )
 }
